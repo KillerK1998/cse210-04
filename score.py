7d8f177ce3c70ccb9d53a9729c7d62cd0ef54f1c
emoryhubbardbyui <hub21003@byui.edu>
2022-11-04 22:24:26 -0400

    Little smaller score font, prefixed with "Score:"

--- score.py
+++ score.py
@@ -5,10 +5,10 @@ class Score(Drawable):
         super().__init__(output_service)
         self.value = 0
         self.update_score(self.value)
-        self.set_font_size(50)
+        self.set_font_size(25)
     def display_score(self):
         #displays current scores
         print(f"Score: {self.value}")
     def update_score(self, points):
         self.value += points
-        self._text = f"{self.value}"
+        self._text = f"Score: {self.value}"
