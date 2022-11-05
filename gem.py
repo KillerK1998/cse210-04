16640ea1b37fabb0a1d18bcb2e755ee2f02dc801
emoryhubbardbyui <hub21003@byui.edu>
2022-11-04 20:47:31 -0400

    made gems bigger

--- gem.py
+++ gem.py
@@ -8,7 +8,7 @@ class Gem(Drawable):
 
         super().__init__(output_service)
         self._text = "*"
-        self._font_size = 30
+        self._font_size = 25
         self._points = 1
         self._position = Point(random.randint(0, 1500), 0)
         print(f"{self._position.x} {self._position.y}")
